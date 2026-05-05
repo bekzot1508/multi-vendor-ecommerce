c2829c3ee5c0bcaf1ac8e898953aa98f3031e30a
bekzot1508 <125878505+bekzot1508@users.noreply.github.com>
2026-05-05 12:45:47 +0500

    CSRF_TRUSTED_ORIGINS is fixed

--- config/settings/base.py
+++ config/settings/base.py
@@ -11,7 +11,9 @@ ALLOWED_HOSTS = config(
     default="127.0.0.1,localhost",
 ).split(",")
 
-CSRF_TRUSTED_ORIGINS=https://multi-vendor-ecommerce-0z5b.onrender.com
+CSRF_TRUSTED_ORIGINS = [
+    "https://multi-vendor-ecommerce-0z5b.onrender.com"
+]
 
 
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
